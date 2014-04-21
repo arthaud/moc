e1eaeacab52502d5a31ac57d7d3815dbe62580d5
Etienne Lebrun <etilebrun@hotmail.com>
2014-04-21 21:18:25 +0200

    ajouts sur IMachine

--- moc/cg/IMachine.java
+++ moc/cg/IMachine.java
@@ -15,4 +15,23 @@ public interface IMachine {
      * Writes the code in a file from the source file name and the suffix
      */
     void writeCode(String fileName, String code) throws MOCException;
-}
+
+    Code genFunction(String label, int paramSize, int retSize, Code code);
+
+    Code genConditional(Code condition, Code trueBloc, Code falseBloc);
+
+    Code genReturn(Code returnVal);
+
+    Code includeAsm(String asmCode);
+    
+    Code genAffectation(Code Adress, Code affectedVal);
+
+    Code genBinary(Code leftOperand, Code rightOperand, String Operator);
+
+    Code genUnary(Code Operand, String Operator);
+
+    Code genCast(DType type, Code CastedCode);
+
+    Code genCall(String ident, Code arguments);
+     
+   }
