4d3f28b06f640507fa0d6169122c5883f5f613d8
Maxime Arthaud <maxime@arthaud.me>
2015-02-08 14:36:42 +0100

    Add more registers in MCRAPS

--- moc/cg/MCRAPS.java
+++ moc/cg/MCRAPS.java
@@ -22,7 +22,8 @@ import moc.type.TINTEGER;
 public class MCRAPS extends AbstractMachine {
 
     public static final String[] registerNames = {
-        "%r1", "%r2", "%r3", "%r4", "%r5", "%r6", "%r7", "%r8", "%r9"
+        "%r1", "%r2", "%r3", "%r4", "%r5", "%r6", "%r7", "%r8", "%r9", "%r10",
+        "%r11", "%r12", "%r13", "%r14", "%r15", "%r16", "%r17", "%r18", "%r19"
     };
 
     public static final HashMap<Long, String> fixedRegisters;
